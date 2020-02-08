050540b92cc91dbe65af66cc2e158fcf93366216
Curtis pachtinger <cpach96@gmail.com>
2020-02-08 04:53:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :name, :grade
+  attr_reader :school_name
+  
+  
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end
+
+  def add_student(name,grade)
+    @roster[grade] = []
+    @roster[grade] << name
+    
+    if @roster.has_key?(grade)
+      
+  end
+
+ 
+end
